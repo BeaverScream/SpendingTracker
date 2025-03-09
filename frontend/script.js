44b87c8613171ecc6b46f8f46585185254b77035
BeaverScream <creamso72@gmail.com>
2025-03-08 16:20:25 -0800

    minor update on script.js file

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,20 @@
-document.getElementById('scrapeForm').addEventListener('submit', async (e) => {
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('scrapeForm').addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-  });
+        let spreadsheetId = document.getElementById("spreadsheetId").value;
+        let user = document.getElementById("user").value;
+        let submitButton = document.querySelector("button[type='submit']");
+
+        if(!spreadsheetId || !user) {
+            showNotification("Please provide the spreadsheet ID and/or the user.");
+            return;
+        }
+
+        console.log(spreadsheetId);
+        console.log(user);
+        console.log(submitButton);
+        console.log("Start scrapping button clicked");
+    });
+ }
+)
